f5edb4e319f4981cee07d5a62b6c44d807cd249e
alan bount <alan@zeroasterisk.com>
2013-09-27 13:02:10 -0400

    Expandable now has an encode() and decode() values
    I had written before/after callbacks on the Expandable models and on the parent model,
    but upon refacotring it just made a lot more sense to do data translations here,
    where we are doing the trasnaltion.
    
    Current support for:
    * restricted_keys: simple array of fieldnames to ignore
    * encode_date: takes fieldname as key, and format as value
    * encode_csv: simple array of fieldnames we want to encode as CSV (handy)
    * encode_json: boolean (support for all other arrays, etc)

--- Test/Case/Model/Behavior/ExpandableBehaviorTest.php
+++ Test/Case/Model/Behavior/ExpandableBehaviorTest.php
@@ -10,7 +10,19 @@ App::uses('AppModel', 'Model');
  */
 class User extends AppModel {
 	public $name = 'User';
-	public $actsAs = array('Expandable.Expandable' => array('with' => 'UserExpand'), 'Containable');
+	public $actsAs = array(
+		'Expandable.Expandable' => array(
+			'with' => 'UserExpand',
+			'encode_json' => true,
+			'encode_csv' => array('states'),
+			'encode_date' => array(
+				'birthdate' => 'Y-m-d',
+				'birthday' => 'm/d',
+			),
+			'restricted_keys' => array('password', 'password_confirm'),
+		),
+		'Containable'
+	);
 	public $hasMany = array('Expandable.UserExpand');
 	public $recursive = -1;
 }
@@ -63,6 +75,19 @@ class ExpandableBehaviorTest extends CakeTestCase {
 		$user['User']['extraField3'] = false;
 		$user['User']['extraField5'] = null;
 		$user['User']['extraField6'] = '';
+		// restricted
+		$user['User']['password_confirm'] = 'Shouldnt be here';
+		// date fields
+		$user['User']['birthdate'] = array('year' => '1979', 'month' => '12', 'day' => '31');
+		$user['User']['birthday'] = array('month' => '12', 'day' => '31');
+		// CSV fields
+		$user['User']['states'] = array('NY', 'CA', 'OH', 'KY', 'CT');
+		// misc array
+		$miscJsArray = array('one', 'two', 3, 4, true, false, null, '');
+		$user['User']['miscJsArray'] = $miscJsArray;
+		$miscJsObject = array('one' =>  'One', 'two' => 'Two', 3 => 3, 4 => 4, 'true' => true, 'false' => false, 'null' => null, '');
+		$user['User']['miscJsObject'] = $miscJsObject;
+
 		$this->User->create(false);
 		$saved = $this->User->save($user);
 		$this->assertFalse(empty($saved));
@@ -74,15 +99,31 @@ class ExpandableBehaviorTest extends CakeTestCase {
 		$userWithExpand = $this->User->find('first', array('contain' => 'UserExpand'));
 		$this->assertNotEqual($userInit, $userWithExpand);
 		// now we can test the values directly on the User model results
-		$this->assertEquals('extraValue1', $user['User']['extraField1']);
-		$this->assertEquals(true, $user['User']['extraField2']);
-		$this->assertEquals(false, $user['User']['extraField3']);
-		$this->assertEquals(null, $user['User']['extraField5']);
-		$this->assertEquals('', $user['User']['extraField6']);
+		$this->assertEquals('extraValue1', $userWithExpand['User']['extraField1']);
+		$this->assertEquals(true, $userWithExpand['User']['extraField2']);
+		$this->assertEquals(false, $userWithExpand['User']['extraField3']);
+		$this->assertEquals(null, $userWithExpand['User']['extraField5']);
+		$this->assertEquals('', $userWithExpand['User']['extraField6']);
+		// restricted
+		$this->assertTrue(empty($userWithExpand['User']['password_confirm']));
+		// date fields
+		$this->assertEquals('1979-12-31', $userWithExpand['User']['birthdate']);
+		$this->assertEquals('12/31', $userWithExpand['User']['birthday']);
+		// CSV fields
+		$this->assertEquals('NY,CA,OH,KY,CT', $userWithExpand['User']['states']);
+		// misc array
+		$this->assertEquals($miscJsArray, $userWithExpand['User']['miscJsArray']);
+		$this->assertEquals($miscJsObject, $userWithExpand['User']['miscJsObject']);
 		// the hasMany relationship is passed through... but we are removing
 		// for simpler full-array tesing
 		$this->assertFalse(empty($userWithExpand['UserExpand']));
 		unset($userWithExpand['UserExpand']);
+		// convert a few transformed values
+		$user['User']['extraField2'] = 1; // transformed on save?
+		$user['User']['birthdate'] = '1979-12-31';
+		$user['User']['birthday'] = '12/31';
+		$user['User']['states'] = implode(',', $user['User']['states']);
+		unset($user['User']['password_confirm']);
 		$this->assertEqual($user, $userWithExpand);
 	}
 }
